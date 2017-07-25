59d209145a003abd2051ce107e8494f776ea656d
Austin Bullard <abullard@basecampcodingacademy.org>
2017-07-25 10:54:24 -0500

    Starting New Store

---
+++ store_core.py
@@ -0,0 +1,7 @@
+def product_name(product):
+    if product == '1' or product.lower() == 'b.w':
+        return product = 'Barbed Wire'
+    else:
+        return ('\nSorry, We don\'t have this\n')
+    
+    
